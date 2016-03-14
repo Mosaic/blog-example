1343cbf1b1f841f10ea3b0db48d9ae451d743134
Patrick Brouwers <patrickbrouwersfilm@gmail.com>
2016-03-14 13:25:05 +0100

    Use fallback handler for view exception handler

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,29 +1,80 @@
 <?php
 
-/**
- * Init application
- */
+/*
+|--------------------------------------------------------------------------
+| Init Application
+|--------------------------------------------------------------------------
+*/
 
 $app = new Mosaic\Cement\Application(
     realpath(__DIR__ . '/../')
 );
 
-$app->components(
-    Mosaic\Http\Component::diactoros(),
-    Mosaic\Routing\Component::fastRoute()->binders(
-        new App\Http\Routes\HomeRoute,
-        new App\Http\Routes\ArticleRoute
-    ),
-    Mosaic\Filesystem\Component::flystem($app->getFolderStructure())->local(
-        'blog',
-        $app->getFolderStructure()->storagePath() . '/blog'
-    ),
+/*
+|--------------------------------------------------------------------------
+| Exception handling component
+|--------------------------------------------------------------------------
+*/
+
+$app->component(Mosaic\Exceptions\Component::whoops()->formatters(
+    function ($exception) use ($app) {
+        $handler = new App\Exceptions\ViewPageExceptionHandler(
+            $app->getContainer()->get(Mosaic\View\Factory::class)
+        );
+
+        $handler->setException($exception);
+
+        return $handler->handle();
+    }
+));
+
+/*
+
+/*
+|--------------------------------------------------------------------------
+| Http component
+|--------------------------------------------------------------------------
+*/
+
+$app->component(Mosaic\Http\Component::diactoros());
+
+/*
+|--------------------------------------------------------------------------
+| Routing component
+|--------------------------------------------------------------------------
+*/
+
+$app->component(Mosaic\Routing\Component::fastRoute()->binders(
+    new App\Http\Routes\HomeRoute,
+    new App\Http\Routes\ArticleRoute
+));
+
+/*
+|--------------------------------------------------------------------------
+| Filesystem component
+|--------------------------------------------------------------------------
+*/
+
+$app->component(Mosaic\Filesystem\Component::flystem($app->getFolderStructure())->local(
+    'blog',
+    $app->getFolderStructure()->storagePath() . '/blog'
+));
+
+/*
+|--------------------------------------------------------------------------
+| View component
+|--------------------------------------------------------------------------
+*/
+
+$app->component(
     Mosaic\View\Component::twig($app->getFolderStructure())
 );
 
-$app->component(Mosaic\Exceptions\Component::whoops()->formatters(
-    new App\Exceptions\ViewPageExceptionHandler($app->getContainer()->get(Mosaic\View\Factory::class))
-));
+/**
+ * |--------------------------------------------------------------------------
+ * | Providers
+ * |--------------------------------------------------------------------------
+ */
 
 $app->provide(new App\Providers\MarkdownProvider);
 $app->provide(new App\Providers\RepositoryProvider);
